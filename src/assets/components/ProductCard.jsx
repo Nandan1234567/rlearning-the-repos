d80293b12d4c0a6fd3f3e08d97aca7ce28c07dd6
uma shankar <umashankar.kousha@gmail.com>
2025-11-18 23:25:20 +0530

    Changed card flex type

--- src/assets/components/ProductCard.jsx
+++ src/assets/components/ProductCard.jsx
@@ -4,22 +4,22 @@ const ProductCard = (props) => {
   let { title, image, price, rating } = props;
   return (
     <div>
-      <div className=" flex justify-between flex-col group relative shadow-md p-3 rounded-md h-full">
+      <div className=" flex justify-between flex-col  shadow-lg hover:cursor-pointer p-3 rounded-md h-100 w-70 mt-5">
         <img
           src={image}
           alt="Front of men's Basic Tee in dark gray"
           className="aspect-square w-full rounded-md "
         />
-        <div className="mt-4 flex flex-col justify-between">
+        <div className="mt-4 flex flex-col justify-between ">
           <h3 className="text-sm text-gray-800">{title}</h3>
-          <div className="flex justify-between">
+          <div className="flex justify-between mt-2 items-center">
             <p className="text-sm font-medium text-gray-900">$ {price}</p>
-            <p className="mt-1 text-sm text-gray-500">Rating : {rating.rate}</p>
+            <p className="mt-1 text-sm text-white bg-green-700 p-1 w-7 text-center">
+              {" "}
+              {rating.rate}
+            </p>
           </div>
         </div>
-        <button className="w-5/6 ml-auto mr-auto bg-blue-500 hover:bg-blue-600 hover:cursor-pointer rounded-md p-1 mt-2 text-white">
-          Add To Cart
-        </button>
       </div>
     </div>
   );
