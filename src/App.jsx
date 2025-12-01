3a093e0911335ca0d7047c5511bae036e1a4f87c
Nandan1234567 <shettynandan071@gmail.com>
2025-12-01 15:49:45 +0530

    removed un used file

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import "./App.css";
-
-import ProductsListing from "./assets/components/ProductsListing";
-import products from "./assets/data/productsData";
-import Navbar from "./assets/components/Navbar";
 import DetailedProduct from "./assets/components/DetailedProduct";
+import Navbar from "./assets/components/Navbar";
+import ProductsListing from "./assets/components/ProductsListing";
 
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
+import { useState } from "react";
 import Cart from "./assets/components/Cart";
 import NotFound from "./assets/components/NotFound";
 import CartContext from "./assets/context/CartContext";
-import CartItem from "./assets/components/CartItem";
-import { useState } from "react";
 
 const App = () => {
   const [cartData, updateCartData] = useState([]);
